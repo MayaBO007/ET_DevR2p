f66f66de3bbb85c38f83788091670cfa276af524
Maya <mayabaror@gmail.com>
2023-09-02 22:00:50 +0300

    דד

--- dayOrganizer.js
+++ dayOrganizer.js
@@ -12,7 +12,7 @@ function moveToDay() {
             const problem = document.getElementById("problem");
             const problem_hor = document.getElementById("problem_hor");
 
-            if (todayDate == "01") {
+            if (todayDate == "13") {
                 platform.goToUrl("days/twoTests/twoTests.html");
             } else if (typeof studySessionData === "undefined" || studySessionData.doneInstructions === "") {
                 platform.goToUrl("instructions/instructions.html");
