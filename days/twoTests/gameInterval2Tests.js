95f503d4fdf3e10a8e1ef3656fc778317edd6610
Maya <mayabaror@gmail.com>
2023-09-15 06:38:15 +0300

    ss

--- days/twoTests/gameInterval2Tests.js
+++ days/twoTests/gameInterval2Tests.js
@@ -139,7 +139,7 @@ async function startInterval2Tests() {
             reset_airplane();
             // reset_redCar();
             // reset_blueCar();
-        }, 20000);
+        }, 5000);
         // }, 3000);
     });
 };
@@ -244,7 +244,7 @@ async function startInterval2Tests2() {
                 })
             };
             savingTrainingThree2()
-        }, 30000);
+        }, 7000);
         // }, 3000);
     });
 };
