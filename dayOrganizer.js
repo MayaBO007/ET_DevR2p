3eda81a9654f0ee325bc3d873e8e8837f595a097
Maya <mayabaror@gmail.com>
2023-12-18 10:40:21 +0200

    ss

--- dayOrganizer.js
+++ dayOrganizer.js
@@ -12,7 +12,7 @@ function moveToDay() {
             const problem = document.getElementById("problem");
             const problem_hor = document.getElementById("problem_hor");
 
-            if (todayDate == "15") {
+            if (todayDate == "19") {
                 platform.goToUrl("days/twoTests/twoTests.html");
             } else if (typeof studySessionData === "undefined" || studySessionData.doneInstructions === "") {
                 platform.goToUrl("instructions/instructions.html");
