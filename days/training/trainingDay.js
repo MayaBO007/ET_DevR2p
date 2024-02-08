885cbb525f84637365140cf0967d0d40d194131d
Maya <mayabaror@gmail.com>
2024-02-08 09:25:34 +0200

    context menu disable

--- days/training/trainingDay.js
+++ days/training/trainingDay.js
@@ -15,13 +15,21 @@ const responsesTrainingData = {
     allChoices: allChoices
 };
 
+var redElement = document.getElementById("redButton");
+var blueElement = document.getElementById("blueButton");
+
 document.getElementById("redButton").addEventListener("click", function () {
     allRedPresses.push(new Date().getTime() - milliseconds);
 });
 document.getElementById("blueButton").addEventListener("click", function () {
     allBluePresses.push(new Date().getTime() - milliseconds);
 });
-
+redElement.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+});
+blueElement.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+});
 
 let saveAttemptTraining = 0;
 let count = 0; // counter for iterations
